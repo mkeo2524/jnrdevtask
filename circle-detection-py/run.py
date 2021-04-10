69f0736ed8878cf8ab322d8edcb3b82fde8c518f
Michael Eo <mkeo2524@gmail.com>
2021-04-10 13:45:54 +1200

    Add main python function

--- circle-detection-py/run.py
+++ circle-detection-py/run.py
@@ -2,21 +2,40 @@
 This file is used to solve each problem
 given in the task. 
 """
+
+import sys
 import runTask as rt
+'''
+pair 1-12
+for runP1 
 
-# define pair
-pair = 'pair1'
+'''
+def validate_inputs(params):
+    print('')
+    return 
 
-# define A or B (for runP1)
-im = 'B'
+if __name__ == "__main__":
+    
+    validate_inputs(sys.argv)
+    # define pair
+    pair = 'pair1'
 
-# define image set (for runP3)
-# 2 is used for pair4-7
-# 3 is used for pair8-12
-imSet = '3'
+    # define A or B (for runP1)
+    im = 'B'
 
-# define which problem to solve
-p = 1
+    # define image set (for runP3)
+    # 2 is used for pair4-7
+    # 3 is used for pair8-12
+    imSet = '3'
 
-rt.runP1(pair, im) if p == 1 else rt.runP2(pair) if p == 2 else rt.runP3(pair, imSet)
+    # define which problem to solve
+    p = 1
+
+    if p == 1:
+        rt.runP1(pair, im)
+    elif p == 2:
+        rt.runP2(pair)
+    else:
+        rt.runP3(pair,imSet)
+    rt.runP1(pair, im) if p == 1 else rt.runP2(pair) if p == 2 else rt.runP3(pair, imSet)
 
