20a712b275cb7e99592c728cf3df486e2724aceb
michael <meo482@aucklanduni.ac.nz>
2021-04-11 12:22:46 +1200

    fixed argument input

--- circle-detection-py/run.py
+++ circle-detection-py/run.py
@@ -5,23 +5,35 @@ given in the task.
 
 import sys
 import runTask as rt
+import getopt
 '''
 pair 1-12
 for runP1 
-
+python run.py problem pair image# P3SET
+python run.py   P1,2or3    1-12  A or B  1,2 or 3
 '''
 def validate_inputs(params):
     print('')
     return 
 
 if __name__ == "__main__":
-    
+    problem_num = None
+    pair = None
+    image_num = None
+    P3_test = None
     validate_inputs(sys.argv)
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, '', ["problem_num =",
+                                          "pair =",
+                                          "image_num =",
+                                          "P3_test ="])
+    print(args)
+    print(opts)
     # define pair
-    pair = 'pair1'
+    pair = 'pair11'
 
     # define A or B (for runP1)
-    im = 'B'
+    imageNo = 'B'
 
     # define image set (for runP3)
     # 2 is used for pair4-7
@@ -30,12 +42,12 @@ if __name__ == "__main__":
 
     # define which problem to solve
     p = 1
-
+'''
     if p == 1:
-        rt.runP1(pair, im)
+        rt.runP1(pair, imageNo)
     elif p == 2:
         rt.runP2(pair)
     else:
         rt.runP3(pair,imSet)
-    rt.runP1(pair, im) if p == 1 else rt.runP2(pair) if p == 2 else rt.runP3(pair, imSet)
-
+    rt.runP1(pair, imageNo) if p == 1 else rt.runP2(pair) if p == 2 else rt.runP3(pair, imSet)
+'''
